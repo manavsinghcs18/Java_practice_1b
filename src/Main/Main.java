ec7034bc7f05387e3bf5442e5bcd3fa5bcb947a1
DELL <manav.singh_cs18@gla.ac.in>
2019-08-24 13:51:00 +0530

    Initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,18 @@
+package Main;
+/**
+ * @author ManavSingh
+ * @version 1.0*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        String n;
+        System.out.println("Enter the string");
+        n=scanner.next();
+        System.out.println("GLA " + n + " Mathura");
+    }
+}
